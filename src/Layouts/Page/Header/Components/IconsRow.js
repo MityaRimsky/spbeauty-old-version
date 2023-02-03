4b46e1869ac963096bee3f4729d4f8fcd2611952
MityaRimsky <123651254+MityaRimsky@users.noreply.github.com>
2023-02-03 10:16:17 +0000

    Update 03.02.2023

--- src/Layouts/Page/Header/Components/IconsRow.js
+++ src/Layouts/Page/Header/Components/IconsRow.js
@@ -5,18 +5,10 @@ import NotificationsIcon from "../../../../Icons/NotificationsIcon";
 import IconOnlyButton from "../../../../Components/IconOnlyButton";
 
 const ButtonLeftWrapper = styled.div`
-    color: #D3EAFE;
     padding-right:6px;
-    &:hover {
-        opacity: 80%;
-    }
 `;
 const ButtonRightWrapper = styled.div`
-    color: #D3EAFE;
     padding-left:6px;
-    &:hover {
-        opacity: 80%;
-    }
 `;
 
 function IconsRow() {
